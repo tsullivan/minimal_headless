01f4c5ed850bc78100e3bbb7e6da94e2fd31e994
Timothy Sullivan <tsullivan@elastic.co>
2022-10-01 21:32:22 -0700

    setup

--- run.ts
+++ run.ts
@@ -4,7 +4,9 @@ import * as puppeteer from 'puppeteer';
   const browser = await puppeteer.launch({
     executablePath: './headless_shell-linux_x64/headless_shell',
     userDataDir: 'userdata',
-    args: [],
+    args: [
+      '--ozone-platform=drm',
+    ],
   });
 
   const page = await browser.newPage();
@@ -14,10 +16,18 @@ import * as puppeteer from 'puppeteer';
     console.log(`console-${type}`, text);
   });
 
+  page.on('error', (err) => {
+    console.error(err);
+  });
+
   await page.goto('https://webglsamples.org/aquarium/aquarium.html', {
     waitUntil: 'load',
   });
 
+  // try to avoid capturing too soon
+  await new Promise((resolve) => setTimeout(resolve, 1200));
+
+  // capture the page
   await page.screenshot({ path: 'aquarium.png' });
 
   await browser.close();
--- setup.sh
+++ setup.sh
@@ -1,10 +1,13 @@
+mkdir -p artifacts
+
 gsutil -m cp \
   "gs://headless_shell_staging/chromium-4fe4891-locales-linux_x64.md5" \
   "gs://headless_shell_staging/chromium-4fe4891-locales-linux_x64.zip" \
-  .
+  ./artifacts
 
-unzip chromium-4fe4891-locales-linux_x64.zip  
+unzip -f artifacts/chromium-4fe4891-locales-linux_x64.zip
 
-PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=1 yarn
+PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=1 \
+  yarn
 
 echo 'setup complete'
